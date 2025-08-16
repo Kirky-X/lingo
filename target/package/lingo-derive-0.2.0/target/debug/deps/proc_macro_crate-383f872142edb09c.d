ed439c81812c82c9f20d3847379b97583ae6ebe5
Kirky-X <kirky-x@outlook.com>
2025-08-16 14:03:24 +0800

    chore(docs/ci): update CONTRIBUTING, add CLA and CLA workflow; add rustfmt.toml

---
+++ target/package/lingo-derive-0.2.0/target/debug/deps/proc_macro_crate-383f872142edb09c.d
@@ -0,0 +1,7 @@
+D:\project\lingo\target\package/lingo-derive-0.2.0\target\debug\deps\libproc_macro_crate-383f872142edb09c.rmeta: D:\SDK\rust\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro-crate-3.3.0\src\lib.rs
+
+D:\project\lingo\target\package/lingo-derive-0.2.0\target\debug\deps\libproc_macro_crate-383f872142edb09c.rlib: D:\SDK\rust\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro-crate-3.3.0\src\lib.rs
+
+D:\project\lingo\target\package/lingo-derive-0.2.0\target\debug\deps\proc_macro_crate-383f872142edb09c.d: D:\SDK\rust\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro-crate-3.3.0\src\lib.rs
+
+D:\SDK\rust\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro-crate-3.3.0\src\lib.rs:
---
+++ target/package/lingo-derive-0.2.0/target/debug/lingo_derive.d
@@ -0,0 +1,1 @@
+D:\project\lingo\target\package/lingo-derive-0.2.0\target\debug\lingo_derive.dll: D:\project\lingo\target\package\lingo-derive-0.2.0\src\lib.rs
